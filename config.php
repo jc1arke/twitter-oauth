b922f8a36282874583053d0ffed7cf052a3a5578
John Clarke <john@jc-interactive.co.za>
2010-10-04 09:38:11 +0200

    Config updated

--- config.php
+++ config.php
@@ -6,9 +6,9 @@
 |
 */
 $config["twitter"] = array(
-	"twitter_api_key" 				=> "WJPYXWnwLgAZTEuuh8Z9aA", 														// The @Anywhere key for Twitter API (get yours via http://dev.twitter.com/apps)
-	"twitter_consumer_secret"	=> "gQIrEcKLMqZgJuHN81iaJBr5Bj3sjcxAPRtc0cLci0",				// The consumer secret key that is associated with this application
-	"twitter_callback_url"  	=> "http://php.tuts.local/week8/twitter.callback.php" 	// The URL that the Twitter API would return to upon completion of the AUTH request
+	"twitter_api_key" 				=> "", 							// The @Anywhere key for Twitter API (get yours via http://dev.twitter.com/apps)
+	"twitter_consumer_secret"	=> "",							// The consumer secret key that is associated with this application
+	"twitter_callback_url"  	=> "callback.php" 	// The URL that the Twitter API would return to upon completion of the AUTH request
 );
 
 /* End of file: config.php */
